ad024d720fbd33df03137da51a7d397a326649d4
orvice <orvice@gmail.com>
2014-08-16 14:17:15 +0800

    添加注册叶

---
+++ register.php
@@ -0,0 +1,54 @@
+<?php
+
+
+//引用数据库连接文件
+require_once 'lib/config.php';
+
+//存在用户cookie，跳转到首页
+if(!empty($_COOKIE['user_name'])){
+    header("location:index.php");
+}
+
+if(!empty($_POST)){
+
+//获取_POST并赋值
+    $username = $_POST['username'];
+    $pwd      = md5($_POST['password']); //md5加密
+
+
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>注册</title>
+    <?php include_once 'lib/header.inc.php';?>
+</head>
+<body>
+
+<?php include_once 'lib/nav.php'; ?>
+
+<!-- Custom styles for singin -->
+<link href="css/signin.css" rel="stylesheet">
+
+<!-- Begin page content -->
+<div class="container">
+    <div class="page-header">
+        <h1>注册</h1>
+    </div>
+    <form class="form-signin" role="form" action="register.php" method="post">
+        <input type="username"  name="username" class="form-control" placeholder="Username" required autofocus>
+        <input type="password"  name="password" class="form-control" placeholder="Password" required>
+        <label class="checkbox">
+            <input type="checkbox" value="remember-me"> Remember me
+        </label>
+        <button class="btn btn-lg btn-primary btn-block" type="submit" name="login">提交注册</button>
+    </form>
+</div><!-- /.container -->
+
+<?php include_once 'lib/footer.inc.php';?>
+</body>
+</html>
+
