be3d99be647824827ad6ff24c1cac21643a2d499
orvice <orvice@gmail.com>
2014-09-01 19:37:34 +0800

    update link using BD_Path

--- lib/nav.php
+++ lib/nav.php
@@ -7,7 +7,7 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="index.php"> <?php echo $site_name; ?> </a>
+            <a class="navbar-brand" href="/<?php echo BD_PATH; ?>index.php"> <?php echo $site_name; ?> </a>
         </div>
         <div class="navbar-collapse collapse">
             <ul class="nav navbar-nav navbar-right">
@@ -24,8 +24,8 @@
                 <?php
                 //未登录显示登录注册
                 if(empty($_COOKIE[user_name])) {    ?>
-                <li><a href="register.php">注册</a></li>
-                <li><a href="login.php">登录</a></li>
+                <li><a href="/<?php echo BD_PATH; ?>register.php">注册</a></li>
+                <li><a href="/<?php echo BD_PATH; ?>login.php">登录</a></li>
                 <?php } ?>
 
             </ul>
