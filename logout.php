9f0935f8bd64834de2f872f7aab33670ff250914
orvice <orvice@gmail.com>
2014-09-01 14:47:41 +0800

    update

--- logout.php
+++ logout.php
@@ -12,7 +12,7 @@ session_start();
 if($_GET['action'] == "logout"){
     //unset($_SESSION['user_id']);
     //unset($_SESSION['user_name']);
-    setcookie("user_name", $user_name, time()-3600);
+    setcookie("user_name", "", time()-3600);
     header("Location:index.php");
     //echo 'Logout successful <a href="index.php">Index</a>';
     exit;
