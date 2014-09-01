f9df87062d736b36254a96354e9a1ce2c2d7decf
orvice <orvice@gmail.com>
2014-09-01 19:02:28 +0800

    update add func

--- admin/doaddmain.php
+++ admin/doaddmain.php
@@ -3,6 +3,12 @@ $title     = $_POST['title'];
 $downlink  = $_POST['downlink'];
 $downsize  = $_POST['downsize'];
 $downinfo  = $_POST['downinfo'];
+$cate      = $_POST['cate'];
+
+$add_sql   = "INSERT INTO `bd_item` (`item_title`, `item_cate_id`, `item_url`, `item_size`, `item_text`, `item_count`, `item_date`)
+                     VALUES ('$title', '$cate', '$downlink', '$downsize', '$downinfo', '1', now() )";
+
+$query = mysql_query($add_sql);
 
 ?>
 <div class="container" >
@@ -11,12 +17,8 @@ $downinfo  = $_POST['downinfo'];
         <h1>添加下载</h1>
     </div>
 
-
-    <?php
-    echo $title;
-    
-
-    ?>
+    成功添加 <?php
+    echo $title; ?> <a href="index.php?action=item">返回列表</a>
 
     <div id="doadd">
    </div>
