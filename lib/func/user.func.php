7b8ee3f90a520efc2e41ba612a860b0b44501b10
orvice <orvice@gmail.com>
2014-09-04 12:49:08 +0800

    add func

--- lib/func/user.func.php
+++ lib/func/user.func.php
@@ -12,3 +12,29 @@ function username_check($username=""){
         return false;
     }
 }
+
+//写用户到数据库函数，成功返回true
+function reg_to_db($user_name,$user_pwd,$user_email){
+    $reg_sql = "INSERT INTO `bd_user` ( `user_name`, `user_pwd`, `user_email`)
+                VALUES ( '$user_name', '$user_pwd', '$user_email')";
+    $query = mysql_query($reg_sql);
+    if ($query){
+        return true;
+    }
+    else{
+        return false;
+    }
+
+}
+
+//更改密码
+function change_pwd($uid,$new_pwd){
+    $change_pwd_sql = "UPDATE `bd_user` SET user_pwd = '$new_pwd' WHERE uid ='$uid' ";
+    $query = mysql_query($change_pwd_sql);
+    if ($query){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
