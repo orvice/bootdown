83de9636b2da05b2a4f33e3cd158457fd9e31bf1
orvice <orvice@gmail.com>
2014-10-11 01:11:41 +0800

    update link

--- adm/lib/slidebar_left.inc.php
+++ adm/lib/slidebar_left.inc.php
@@ -42,7 +42,7 @@
 
                     <ul class="treeview-menu">
                         <li><a href="item_manage.php"><i class="fa fa-angle-double-right"></i> 管理</a></li>
-                        <li><a href="down_add.php"><i class="fa fa-angle-double-right"></i> 添加</a></li>
+                        <li><a href="itme_add.php"><i class="fa fa-angle-double-right"></i> 添加</a></li>
                     </ul>
                 </li>
 
@@ -53,8 +53,8 @@
                         <i class="fa fa-angle-left pull-right"></i>
                     </a>
                     <ul class="treeview-menu">
-                        <li><a href="down_mana.php"><i class="fa fa-angle-double-right"></i> 管理</a></li>
-                        <li><a href="down_add.php"><i class="fa fa-angle-double-right"></i> 添加</a></li>
+                        <li><a href="cate_manage.php"><i class="fa fa-angle-double-right"></i> 管理</a></li>
+                        <li><a href="cate_add.php"><i class="fa fa-angle-double-right"></i> 添加</a></li>
                     </ul>
                 </li>
 
