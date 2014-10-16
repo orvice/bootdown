dbfdc95060dc63459f2e869d69bdcbaeabdb1aed
orvice <orvice@gmail.com>
2014-10-16 15:35:10 +0800

    new login page

--- admin/login.php
+++ admin/login.php
@@ -30,38 +30,57 @@ $query = $dbc->query($check_query);
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Login</title>
-    <?php include_once '../lib/header_admin.inc.php';?>
-    <!-- Custom styles for singin -->
-    <link href="../css/signin.css" rel="stylesheet">
+    <title>登录</title>
+    <!-- bootstrap 3.0.2 -->
+    <link href="../bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- font Awesome -->
+    <link href="../css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <!-- Theme style -->
+    <link href="../css/AdminLTE.css" rel="stylesheet" type="text/css" />
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
 </head>
-<body>
 
-<?php include_once '../lib/nav.php'; ?>
 
-<!-- Begin page content -->
-<div class="container">
-    <div class="page-header">
-        <h1>登录</h1>
-    </div>
-    <form class="form-signin" role="form" action="login.php" method="post">
-        <div class="form-group">
-        <input type="username"  name="username" class="form-control" placeholder="Username" required autofocus>
+<body class="bg-black">
+
+<div class="form-box" id="login-box">
+
+    <div class="header">登录</div>
+      <form role="form" action="login.php" method="post">
+        <div class="body bg-gray">
+            <div class="form-group">
+                <input type="username"  name="username" class="form-control" placeholder="Username" required autofocus>
+            </div>
+            <div class="form-group">
+                <input type="password"  name="password" class="form-control" placeholder="Password" required>
+            </div>
+            <div class="form-group">
+                <input type="checkbox" name="remember_me"/> Remember me
+            </div>
         </div>
+        <div class="footer">
+            <button type="submit" class="btn bg-olive btn-block" type="submit" name="login" >记住登录</button>
 
-        <div class="form-group">
-        <input type="password"  name="password" class="form-control" placeholder="Password" required>
+            <p><a href="#">忘记密码</a></p>
+
         </div>
+      </form>
 
-        <label  class="checkbox" for="checkbox1">
-            <input type="checkbox" value="remember-me" id="checkbox1"  data-toggle="checkbox"> 记住登录
-        </label>
+</div>
 
-        <button class="btn btn-lg btn-primary btn-block" type="submit" name="login">登录</button>
-    </form>
-</div><!-- /.container -->
 
-<?php include_once '../lib/footer_admin.inc.php';?>
+<!-- jQuery 2.0.2 -->
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="../../js/bootstrap.min.js" type="text/javascript"></script>
+
 </body>
+
 </html>
 
