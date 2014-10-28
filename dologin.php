a9cd7f122ec5f7d3160e359937ace829b27319a1
orvice <orvice@gmail.com>
2014-10-28 18:12:08 +0800

    用户密码检测写成函数

--- dologin.php
+++ dologin.php
@@ -11,22 +11,17 @@ header("content-type:text/html;charset=utf-8");
 
 //引用数据库连接文件
 require_once 'lib/config.php';
+require_once 'lib/func/user.func.php';
 
 
 if(!empty($_POST)){
 
-//获取_POST并赋值
+    //获取_POST并赋值
     $username = $_POST['username'];
     $pwd      = md5($_POST['password']); //md5加密
 
-//检测用户名及密码是否正确
-    //$check_query = mysql_query("select uid from bd_user where user_name='$username' and user_pwd='$pwd' limit 1");
-    $check_query = "select * from bd_user where user_name='$username'  limit 1";
-    $query = $dbc->query($check_query);
-    $rs = $query->fetch_array();
-    //读取密码
-    $pw = $rs['user_pwd'];
-    if($pwd == $pw ){
+    $rt = user_login_check($username,$pwd);
+    if($rt == 1){
         setcookie("user_name", $username, time()+3600);
         header("location: index.php ");
         exit;
--- lib/func/user.func.php
+++ lib/func/user.func.php
@@ -1,6 +1,27 @@
 <?php
 //用户相关函数
 
+//登陆香港
+function user_login_check($user,$pwd){
+    global $dbc;
+    $check_query = "SELECT * FROM bd_user WHERE user_name='$user'  limit 1";
+    $query = $dbc->query($check_query);
+    if(!$query){
+        //用户名不存在，返回0
+        return 0;
+    }else{
+        $rs = $query->fetch_array();
+        if($rs['pwd'==$pwd]){
+            //密码正确返回1
+            return 1;
+        }else{
+            //密码错误返回2
+            return 2;
+        }
+    }
+
+}
+
 //检测用户名是否存在函数，存在返回true
 function username_check($username=""){
     $user_check_sql = "SELECT * FROM `bd_user` WHERE user_name= '$username' ";
