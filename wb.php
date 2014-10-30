004641d7a030ee35da26783e2892b55908ba9ba0
orvice <orvice@gmail.com>
2014-10-30 23:04:43 +0800

    提交提交页面

--- wb.php
+++ wb.php
@@ -43,7 +43,7 @@ if(empty($_COOKIE['weibo_id'])){
                         </ul>
                         <div id="myTabContent" class="tab-content">
                             <div class="tab-pane active in" id="login">
-                                <form class="form-horizontal" action='' method="POST">
+                                <form class="form-horizontal" action='wb_do.php' method="POST">
                                     <fieldset>
                                         <div id="legend">
                                             <legend class="">绑定现有账号</legend>
@@ -75,7 +75,7 @@ if(empty($_COOKIE['weibo_id'])){
                                 </form>
                             </div>
                             <div class="tab-pane fade" id="create">
-                                <form id="tab">
+                                <form id="tab" action="wb_do.php" method="post">
                                     <div id="legend">
                                         <legend class="">注册个新账号</legend>
                                     </div>
--- wb_do.php
+++ wb_do.php
@@ -1,7 +1,1 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: orvice
- * Date: 10/30/14
- * Time: 11:02 PM
- */ 
