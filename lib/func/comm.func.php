80dcb3b415b49285a3866334c1090f7d815274c8
orvice <orvice@gmail.com>
2014-11-04 01:18:19 +0800

    add get_gravatar func

--- lib/func/comm.func.php
+++ lib/func/comm.func.php
@@ -59,3 +59,16 @@ function get_cate_sql($id){
     $cate_sql = "SELECT * FROM `bd_cate`  WHERE cate_id = $id";
     return $cate_sql;
 }
+
+function get_gravatar( $email, $s = 80, $d = 'mm', $r = 'g', $img = false, $atts = array() ) {
+    $url = 'http://www.gravatar.com/avatar/';
+    $url .= md5( strtolower( trim( $email ) ) );
+    $url .= "?s=$s&d=$d&r=$r";
+    if ( $img ) {
+        $url = '<img src="' . $url . '"';
+        foreach ( $atts as $key => $val )
+            $url .= ' ' . $key . '="' . $val . '"';
+        $url .= ' />';
+    }
+    return $url;
+}
