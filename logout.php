853e771b91afcd87ba08482590e49628dca1e43e
orvice <orvice@gmail.com>
2014-11-05 20:47:52 +0800

    update logout

--- logout.php
+++ logout.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: orvice
- * Date: 14-7-8
- * Time: 下午10:44
- */
-
 session_start();
 
 //注销登录
@@ -13,7 +6,8 @@ if($_GET['action'] == "logout"){
     //unset($_SESSION['user_id']);
     //unset($_SESSION['user_name']);
     setcookie("user_name", "", time()-3600);
-    header("Location:index.php");
-    //echo 'Logout successful <a href="index.php">Index</a>';
+    setcookie("user_pwd", "", time()-3600);
+    setcookie("user_uid", "", time()-3600);
+    header("Location:index.php"); 
     exit;
 }
