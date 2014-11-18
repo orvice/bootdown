fa651a13c5ba4f59b2139f88a386f78f7742770f
orvice <orvice@gmail.com>
2014-11-18 23:59:44 +0800

    传入数据进行处理防止注入

--- doreg.php
+++ doreg.php
@@ -9,7 +9,13 @@ if(empty($_POST)){
 }
 if(!empty($_POST)){
     //获取_POST并赋值
-    $username = $_POST['username'];
-    $pwd      = md5($_POST['password']); //md5加密
-    $email    = $_POST['email'];
+    $username = mysqli_real_escape_string($dbc,trim($_POST['username']));
+    $pwd      = mysqli_real_escape_string($dbc,trim($_POST['password']));
+    $pwd      = md5($pwd); //md5加密
+    $email    = mysqli_real_escape_string($dbc,trim($_POST['email']));
+}
+//判断用户名是否可用
+if(get_user_uid($username)!=0){
+    echo ' <script>alert("用户名已经被使用!")</script> ';
+    echo " <script>window.location='reg.php';</script> " ;
 }
