fa8be53b132c49eaa8d6c5308c2b25059fc5fa98
orvice <orvice@gmail.com>
2014-11-19 00:07:17 +0800

    逻辑优化

--- doreg.php
+++ doreg.php
@@ -6,22 +6,21 @@ require_once 'lib/func/reg.func.php';
 //非法访问控制
 if(empty($_POST)){
     header("locaion:index.php");
-}
-if(!empty($_POST)){
-    //获取_POST并赋值
+}else{
     $username = mysqli_real_escape_string($dbc,trim($_POST['username']));
     $pwd      = mysqli_real_escape_string($dbc,trim($_POST['password']));
     $pwd      = md5($pwd); //md5加密
     $email    = mysqli_real_escape_string($dbc,trim($_POST['email']));
-}
-//判断用户名是否可用
-if(get_user_uid($username)!=0){
-    echo ' <script>alert("用户名已经被使用!")</script> ';
-    echo " <script>window.location='reg.php';</script> " ;
-}
 
-//判断邮箱是否存在
-if(is_email_ok($email)!=0){
-    echo ' <script>alert("邮箱已经被使用!")</script> ';
-    echo " <script>window.location='reg.php';</script> " ;
+    //判断用户名是否可用
+    if(get_user_uid($username)!=0){
+        echo ' <script>alert("用户名已经被使用!")</script> ';
+        echo " <script>window.location='reg.php';</script> " ;
+    }
+
+    //判断邮箱是否存在
+    if(is_email_ok($email)!=0){
+        echo ' <script>alert("邮箱已经被使用!")</script> ';
+        echo " <script>window.location='reg.php';</script> " ;
+    }
 }
