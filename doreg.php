1ee62143ec5e0be3a26801c16262f3306a4ff960
orvice <orvice@gmail.com>
2014-11-27 01:04:24 +0800

    使用case验证

--- doreg.php
+++ doreg.php
@@ -5,22 +5,26 @@ require_once 'lib/config.php';
 require_once 'lib/func/user.func.php';
 require_once 'lib/func/reg.func.php';
 //非法访问控制
-if(empty($_POST)){
-    header("locaion:index.php");
-}else{
-    //传入表单数据并进行处理
-    $username = mysqli_real_escape_string($dbc,trim($_POST['username']));
-    $pwd      = mysqli_real_escape_string($dbc,trim($_POST['password']));
-    $pwd      = md5($pwd); //md5加密
-    $email    = mysqli_real_escape_string($dbc,trim($_POST['email']));
-    //判断用户名是否可用
-    if(get_user_uid($username)!=0){
+
+$username = mysqli_real_escape_string($dbc,trim($_POST['username']));
+$pwd      = mysqli_real_escape_string($dbc,trim($_POST['password']));
+$pwd      = md5($pwd);
+$email    = mysqli_real_escape_string($dbc,trim($_POST['email']));
+
+switch(empty($_POST)){
+    case !get_user_uid($username):{
         echo ' <script>alert("用户名已经被使用!")</script> ';
-        echo " <script>window.location='register.php';</script> " ;
+        echo " <script>window.location='reg.php';</script> " ;
+        break;
     }
-    //判断邮箱是否存在
-    if(is_email_ok($email)!=0){
+
+    case !is_email_ok($email):{
         echo ' <script>alert("邮箱已经被使用!")</script> ';
-        echo " <script>window.location='register.php';</script> " ;
+        echo " <script>window.location='reg.php';</script> " ;
+        break;
+    }
+
+    default:{
+        echo "ok";
     }
 }
