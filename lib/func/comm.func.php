fdd8b47cfb13c71120b9b80cf685936d44c4dc37
orvice <orvice@gmail.com>
2014-11-27 01:18:25 +0800

    https

--- lib/func/comm.func.php
+++ lib/func/comm.func.php
@@ -61,7 +61,7 @@ function get_cate_sql($id){
 }
 
 function get_gravatar( $email, $s = 80, $d = 'mm', $r = 'g', $img = false, $atts = array() ) {
-    $url = 'http://www.gravatar.com/avatar/';
+    $url = 'https://www.gravatar.com/avatar/';
     $url .= md5( strtolower( trim( $email ) ) );
     $url .= "?s=$s&d=$d&r=$r";
     if ( $img ) {
